2e7d968d591c66344ab0d20ebd2d723c429030a4
fred khoury <fredrick@shift5.io>
2021-05-12 01:07:06 -0500

    it has begun

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const proxyChain = require('proxy-chain');
   puppeteer.use(StealthPlugin());
   puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
   
-  const oldProxyUrl = "http://R4PPiYpcNj:R4PPiYpcNj@us-general-4.resdleafproxies.com:19380";
+  const oldProxyUrl = "http://R4PPiYpcNj:R4PPiYpcNj@us-general-4.resdleafproxies.com:19739";
   const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl);
 
   // Prints something like "http://127.0.0.1:45678"
@@ -18,10 +18,31 @@ const proxyChain = require('proxy-chain');
   });
   // Do your magic here...
   const page = await browser.newPage();
-  await page.setViewport({ width: 800, height: 600 })
 
-  await page.goto("https://whatismyip.com");
-  await page.waitForTimeout(5000)
+  const navigationPromise = page.waitForNavigation()
+  await page.setViewport({ width: 800, height: 600 })
+  await page.goto('https://proshop.innovadiscs.com/customer/account/login/')
+
+  await navigationPromise
+  
+  await page.waitForSelector('#login-form #email')
+  await page.click('#login-form #email')
+  await page.type('#login-form #email', '')
+
+  await page.waitForSelector('.block-slider > .flex-viewport > .slides > .flex-active-slide > .block-content')
+  await page.click('.block-slider > .flex-viewport > .slides > .flex-active-slide > .block-content')
+  
+  await page.waitForSelector('#login-form #pass')
+  await page.click('#login-form #pass')
+  await page.type('#login-form #pass', '')
+
+  await page.waitForSelector('.block-content > #login-form > #send2 > span > span')
+  await page.click('.block-content > #login-form > #send2 > span > span')
+  
+  await navigationPromise
+
+
+  await page.waitForTimeout(10000)
   await page.screenshot({ path: "example.png", fullPage:true });
   await browser.close();
 

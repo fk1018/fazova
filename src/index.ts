76da1370540f15b89ada6276e9ff2856ba7cd853
fred khoury <fredrick@shift5.io>
2021-06-26 22:51:52 -0500

    remove proxy chain

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,38 @@
-const puppeteer = require("puppeteer-extra");
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker')
-const proxyChain = require('proxy-chain');
+import { LaunchOptions } from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import Adblocker from 'puppeteer-extra-plugin-adblocker'
 (async () => {
 
   puppeteer.use(StealthPlugin());
-  puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
-  
-  const oldProxyUrl = "http://R4PPiYpcNj:R4PPiYpcNj@us-general-4.resdleafproxies.com:19739";
-  const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl);
+  puppeteer.use(Adblocker({ blockTrackers: true }))
 
+  const username = 'UKlJFVk7';
+  const password = 'E3RB6e2n2nksbBwF5gYkSd0lRU0HRILBo33q5CvXpEc2VoAImRYR1SC6QjCG5sKPu1tyw-XQvnTj4Q';
+  const proxyIp = 'ustr24.resi.resdleafproxies.com:38661';
   // Prints something like "http://127.0.0.1:45678"
-  console.log(newProxyUrl);
-
-  const browser = await puppeteer.launch({
-    args: [`--proxy-server=${newProxyUrl}`],
-  });
+  // console.log(newProxyUrl);
+  const launchOptions :FaszLaunchOptions = {
+    args: [`--proxy-server=http://${proxyIp}`]
+  }
+  const browser = await puppeteer.launch(launchOptions);
   // Do your magic here...
   const page = await browser.newPage();
 
+  await page.authenticate({username,password});
+
   const navigationPromise = page.waitForNavigation()
   await page.setViewport({ width: 800, height: 600 })
-  await page.goto('https://tilted.io')
+  await page.goto('https://whatismyip.com')
 
   await navigationPromise
 
   await page.waitForTimeout(10000)
   await page.screenshot({ path: "example.png", fullPage:true });
-  await browser.close();
-
-  await proxyChain.closeAnonymizedProxy(newProxyUrl, true);
   
   await browser.close();
 })();
+
+interface FaszLaunchOptions extends LaunchOptions {
+  args: string[]
+}
